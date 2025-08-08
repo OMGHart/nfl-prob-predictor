9ca1573709d6d817a7c1e31fa98b9683d1818a86
hart <mjh912@gmail.com>
2025-08-08 12:46:07 -0400

    Setting sidebar collapse default

--- pages/3_Model_UI.py
+++ pages/3_Model_UI.py
@@ -8,11 +8,22 @@ from utils import logit_func, expit_func
 
 import streamlit as st
 
-st.set_page_config(
-    page_title="NFL Win Probability",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+hide_sidebar_style = """
+    <style>
+        [data-testid="stSidebar"][aria-expanded="true"]{
+            display: none;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"]{
+            display: none;
+        }
+    </style>
+"""
+st.markdown(hide_sidebar_style, unsafe_allow_html=True)
+
 
 
 st.title("Hart's NFL Win Probability Predictor")
